0f9a1ae821063ca2fac175aada41ab0214d9c27d
Vladyslav Herasymiuk <vladyslav.herasymiuk@MacBook-Pro-Vladyslav.local>
2019-01-12 21:43:44 +0200

    first commit

--- main.go
+++ main.go
@@ -1,53 +1,94 @@
 package main
 
 import (
-	"./morse"
 	"bufio"
-	"fmt"
-	"io/ioutil"
+	"encoding/csv"
+	"log"
+	"morse/morse"
 	"os"
-	"strings"
+	"sync"
 )
 
-func ExampleDecodeITU() {
+//func ExampleDecodeITU() {
+//
+//	decoded, _ := morse.DecodeITU("-- .. ... - . .-. / - . -..- - / ..--- ----- ----- -----")
+//	fmt.Print(decoded)
+//}
+//
+//func ExampleEncodeITU() {
+//	f,_ := ioutil.ReadFile("ro-en/europarl-v7.ro-en.en")
+//	data := strings.Split(string(f),"\n")
+//	out, _ := os.Create("morze.txt")
+//	for _, str :=  range data {
+//		encoded := morse.EncodeITU(str)
+//		//fmt.Println("English string: ", str, "\nMorse string: ", encoded)
+//		//encoded =+ "\n"
+//		out.WriteString(encoded)
+//		out.WriteString("\n")
+//
+//	}
+//	//encoded := morse.EncodeITU("mister text 2000")
+//}
+//
+//func t() {
+//	f, _ := os.Open("ro-en/europarl-v7.ro-en.en")
+//	scanner := bufio.NewScanner(f)
+//
+//	out, _ := os.Create("morze_fast.txt")
+//	for scanner.Scan() {
+//		line := scanner.Text()
+//		encoded := morse.EncodeITU(line)
+//		out.WriteString(encoded)
+//		out.WriteString("\n")
+//		//fmt.Println(line)
+//	}
+//}
+//
+//func main(){
+//	//ExampleEncodeITU()
+//	t()
+//	// lol1 := morse.EncodeITU("Membership of Parliament: see Minutes")
+//	// lol, _ := morse.DecodeITU(lol1)
+//	// fmt.Println(lol, " ", lol1)
+//}
 
-	decoded, _ := morse.DecodeITU("-- .. ... - . .-. / - . -..- - / ..--- ----- ----- -----")
-	fmt.Print(decoded)
+type CsvWriter struct {
+	mutex *sync.Mutex
+	csvWriter *csv.Writer
 }
 
-func ExampleEncodeITU() {
-	f,_ := ioutil.ReadFile("ro-en/europarl-v7.ro-en.en")
-	data := strings.Split(string(f),"\n")
-	out, _ := os.Create("morze.txt")
-	for _, str :=  range data {
-		encoded := morse.EncodeITU(str)
-		//fmt.Println("English string: ", str, "\nMorse string: ", encoded)
-		//encoded =+ "\n"
-		out.WriteString(encoded)
-		out.WriteString("\n")
-
+func NewCsvWriter(fileName string) (*CsvWriter, error) {
+	csvFile, err := os.Create(fileName)
+	if err != nil {
+		return nil, err
 	}
-	//encoded := morse.EncodeITU("mister text 2000")
+	w := csv.NewWriter(csvFile)
+	return &CsvWriter{csvWriter:w, mutex: &sync.Mutex{}}, nil
 }
 
-func t() {
+func (w *CsvWriter) Write(row []string) {
+	w.mutex.Lock()
+	w.csvWriter.Write(row)
+	w.mutex.Unlock()
+}
+
+func (w *CsvWriter) Flush() {
+	w.mutex.Lock()
+	w.csvWriter.Flush()
+	w.mutex.Unlock()
+}
+
+func main() {
+	w, err := NewCsvWriter("foo-safe.txt")
+	if err != nil {
+		log.Panic(err)
+	}
 	f, _ := os.Open("ro-en/europarl-v7.ro-en.en")
 	scanner := bufio.NewScanner(f)
-
-	out, _ := os.Create("morze_fast.txt")
 	for scanner.Scan() {
 		line := scanner.Text()
 		encoded := morse.EncodeITU(line)
-		out.WriteString(encoded)
-		out.WriteString("\n")
-		//fmt.Println(line)
+		w.Write([]string{encoded})
 	}
-}
-
-func main(){
-	//ExampleEncodeITU()
-	t()
-	// lol1 := morse.EncodeITU("Membership of Parliament: see Minutes")
-	// lol, _ := morse.DecodeITU(lol1)
-	// fmt.Println(lol, " ", lol1)
+	w.Flush()
 }
